e713432e0dccc902f68ac475a02eaa1f5e27baac
inclementish <55512603+inclementish@users.noreply.github.com>
2019-12-21 15:45:49 -0600

    Add files via upload

---
+++ discourses/jq/discoursesclass.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+$(".copybox2").delay(1000).fadeIn(1000);
+
+$(".copybox3").delay(2000).fadeIn(1000);
+
+});
---
+++ discourses/jq/discoursesenviro.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+// $(window).scroll(function() {
+//    var top = $(".copybox2").scrollTop();
+//    if (top > 100 && top < 130) {
+//        $(".copybox2").stop().animate({
+//           opacity: "1", top: "1400px", left: "0px", bottom: "0"
+//       });
+//    } else {
+
+//        $(".copybox2").stop().animate({
+//            opacity: "0"
+//        }, 'slow');
+//    } 
+//});  i tried...
+
+$(".copybox2").delay(3000).fadeIn(3000);
+
+$(".copybox3").delay(4000).fadeIn(3000);
+
+});
